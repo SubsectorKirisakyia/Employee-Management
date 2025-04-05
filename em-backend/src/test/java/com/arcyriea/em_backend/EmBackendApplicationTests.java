54edfddd8360ed6eb4006e6478ec973a5df51b57
SubsectorKirisakyia <108776309+SubsectorKirisakyia@users.noreply.github.com>
2025-04-05 12:09:13 +0700

    Pushing setup first, since all these was created on the Ramdisk

---
+++ em-backend/src/test/java/com/arcyriea/em_backend/EmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arcyriea.em_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
